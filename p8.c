5f884c733bea6355f1e9705ce10ce6f5d131e6e7
Diwash khatri chhetri <77787660+Diwash3@users.noreply.github.com>
2023-11-28 08:44:43 +0530

    Create p8.c

---
+++ p8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+int main(int argc, char* argv[])
+{
+int size, rank;
+MPI_Init(&argc, &argv);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+MPI_Comm_size(MPI_COMM_WORLD, &size);
+float recvbuf, sendbuf[100];
+if (rank == 0) {
+int i;
+printf("Before Scatter : sendbuf of rank 0 : ");
+for (i = 0; i < size; i++) {
+srand(i);
+sendbuf[i] = (float)(rand()%1000)/10;
+printf("%.1f ", sendbuf[i]);
+}
+printf("\nAfter Scatter :\n");
+}
+MPI_Scatter(sendbuf, 1, MPI_FLOAT, &recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+printf("rank= %d Recvbuf: %.1f\n", rank, recvbuf);
+MPI_Finalize();
+}
+output:
+Before Scatter : sendbuf of rank 0 : 38.3 38.3 29.0 74.6
+After Scatter :
+rank= 0 Recvbuf: 38.3
+rank= 1 Recvbuf: 38.3
+rank= 2 Recvbuf: 29.0
+rank= 3 Recvbuf: 74.6
